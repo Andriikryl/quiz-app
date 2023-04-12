ab5711cfa13f53f8df6c4466ae7da9df1ea39809
Andriikryl <andreyk557@gmail.com>
2023-04-12 21:55:12 +0300

    feat[data-js]

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What animal has the longest lifespan?",
+    answerId: 3,
+    options: [
+      { id: 1, text: "Blue Whale" },
+      { id: 2, text: "Locust" },
+      { id: 3, text: "Locust" },
+      { id: 4, text: "asda" },
+    ],
+  },
+  {
+    id: 2,
+    question: "lorem lorem lorem lorem",
+    answerId: 2,
+    options: [
+      { id: 1, text: "Blue Whale" },
+      { id: 2, text: "Locust" },
+      { id: 3, text: "Locust" },
+      { id: 4, text: "asdf" },
+    ],
+  },
+  {
+    id: 3,
+    question: "What animal has the lorem?",
+    answerId: 2,
+    options: [
+      { id: 1, text: "Blue Whale" },
+      { id: 2, text: "Locust" },
+      { id: 3, text: "Locust" },
+      { id: 4, text: "asff" },
+    ],
+  },
+  {
+    id: 4,
+    question: "Whalorem hlorem span?",
+    answerId: 3,
+    options: [
+      { id: 1, text: "Blue Whale" },
+      { id: 2, text: "Locust" },
+      { id: 3, text: "Locust" },
+      { id: 4, text: "aaaaaa" },
+    ],
+  },
+];
